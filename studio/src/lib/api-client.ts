71fb00f398ed9531b6d0c63e28c22a9f59cdc055
rakesh b <rakeshbsirvi46@gmail.com>
2025-05-11 02:10:56 +0530

    api fixes

--- studio/src/lib/api-client.ts
+++ studio/src/lib/api-client.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:8080/v1", // Replace with your actual base URL
+  baseURL: "https://backend.sasewa.org/v1", // Replace with your actual base URL
   timeout: 10000, // Optional timeout
   headers: {
     "Content-Type": "application/json",
