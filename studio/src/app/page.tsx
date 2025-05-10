d2e13d033bd669c89c6d23ffb348585e2497aeba
rakesh b <rakeshbsirvi46@gmail.com>
2025-05-10 14:40:24 +0530

    fix : redirect to the /playground if already authenticated

--- studio/src/app/page.tsx
+++ studio/src/app/page.tsx
@@ -1,19 +1,27 @@
-"use client"
+"use client";
 
 import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import { createClient } from "@/utils/supabase/client";
 
 function page() {
   const router = useRouter();
 
-  useEffect(()=>{
-    router.push("/login")
-  }, [])
+  useEffect(() => {
+    const fetchSession = async () => {
+      const supabase = createClient();
+      const { data: authUser, error } = await supabase.auth.getUser();
+      if (authUser.user) {
+        router.push("/playground")
+      }
+    };
+
+    fetchSession();
+  }, []);
+
   return (
     <>
-      <div>
-        Landing page
-      </div>
+      <div>Landing page</div>
     </>
   );
 }
