d2dc2a355c4ec386ca2a7da10e80eaac9c6ffe52
rakesh b <rakeshbsirvi46@gmail.com>
2025-05-11 02:10:07 +0530

    ui fixes

--- studio/src/app/login/page.tsx
+++ studio/src/app/login/page.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import AuthPage from "@/components/auth/auth-form";
+import { createClient } from "@/utils/supabase/client";
+import { useRouter } from "next/navigation";
 
 function page() {
+    const router = useRouter();
+
+    useEffect(() => {
+    const fetchSession = async () => {
+      const supabase = createClient();
+      const { data: authUser, error } = await supabase.auth.getUser();
+      if (authUser.user) {
+        router.push("/projects");
+      }
+    };
+
+    fetchSession();
+  }, []);
   return (
     <>
       <AuthPage />
