73b09611fb8261edf413fc20192dbc87fa4b4df4
rakesh b <rakeshbsirvi46@gmail.com>
2025-07-14 17:28:40 +0530

    auth callback issue in vercel deployment

--- studio/src/utils/login/actions.ts
+++ studio/src/utils/login/actions.ts
@@ -2,18 +2,20 @@
 
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
-
 import { createClient } from '@/utils/supabase/server'
 import { Providers } from '@/hooks/auth-context'
 
-export async function login(provider:Providers) {
+export async function login(provider: Providers) {
   const supabase = await createClient()
+
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000'
+  const redirectTo = `${baseUrl}/auth/callback?next=/projects`
 
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: provider,
-    options:{
-        redirectTo: "http://localhost:3000/auth/callback?next=/projects"
-    }
+    options: {
+      redirectTo,
+    },
   })
 
   if (error) {
